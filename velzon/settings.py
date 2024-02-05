a893f307622dd9981581ab94586de1f9ed3c4a66
adeycson <adeycson.psi@gmail.com>
2024-02-05 01:29:02 +0000

    staticfiles e Debug False

--- velzon/settings.py
+++ velzon/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j%^*y0krq5^-#3lggoecxw!d7ad_gqkab3t5w17&0w06+qf8+8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
